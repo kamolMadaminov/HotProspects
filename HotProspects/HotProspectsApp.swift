83d74ec0adfa5f306637eaf6e531dd55095fce9c
developerkamol <developerkamol@gmail.com>
2025-04-16 23:15:23 +0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Kamol Madaminov on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
